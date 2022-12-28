1f6a04103d352b04dda89ffe52c83c26a1997d82
Hippocrates <rimo52@gmail.com>
2022-12-29 01:32:59 +0300

    Добавлен метод заполнения и вывода первоначального массива.

--- Code/Program.cs
+++ Code/Program.cs
@@ -7,3 +7,18 @@ string[] GetArray()
     string[] array = new string[size];
     return array;
 }
+
+// Наполнение массива с клавиатуры и его вывод
+void FillArray(string?[] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write($"Введите элемент массива {i + 1}: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+        i++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Создан первоначальный массив строк. Количество элементов: {i}");
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
