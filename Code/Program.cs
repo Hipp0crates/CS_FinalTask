158b8916651939b4a6b0a584c1da1a727da939f7
Hippocrates <rimo52@gmail.com>
2022-12-29 01:31:00 +0300

    Добавлен метод создания массива строк рандомного размера.

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.Clear();
+
+// Создание массива строк рандомного размера от 3 до 6 элементов
+string[] GetArray() 
+{
+    int size = new Random().Next(3, 7);
+    string[] array = new string[size];
+    return array;
+}
