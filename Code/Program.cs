caedfd269f5196f6c7436eb9c377b4b70db7bcf4
Hippocrates <rimo52@gmail.com>
2022-12-29 01:40:00 +0300

    Добавлен метод определения размера нового массива и заполнения по заданному условию.

--- Code/Program.cs
+++ Code/Program.cs
@@ -22,3 +22,29 @@ void FillArray(string?[] array)
     Console.WriteLine($"Создан первоначальный массив строк. Количество элементов: {i}");
     Console.WriteLine("[" + string.Join(", ", array) + "]");
 }
+
+// Определение размера нового массива, его создание, наполнение по заданному условию и вывод
+void GetNewArray(string[] array)
+{
+    // Определение размера нового массива
+    int newArraySize = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) newArraySize++;
+    }
+    // Создание нового массива
+    string[] newArray = new string[newArraySize];
+    // Наполнение нового массива
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сформирован массив из строк, длинна которых меньше или равна 3 символам. Количество элементов: {newArraySize}");
+    Console.WriteLine("[" + string.Join(", ", newArray) + "]");
+}
